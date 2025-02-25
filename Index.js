5c1909ae3e77dd167f5e46c4e38fc2df04fecb4c
Fieldboss7 <fieldboss7@icloud.com>
2025-02-25 11:43:49 -0600

    Index.js

---
+++ Index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [post, setPost] = useState('');
+  const shareToFacebook = () => {
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}&quote=${encodeURIComponent(post)}`, '_blank');
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>ShareSphere</h1>
+      <textarea value={post} onChange={(e) => setPost(e.target.value)} placeholder="Write your post..." />
+      <button onClick={shareToFacebook}>Share to Facebook</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
